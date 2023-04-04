2c45559870ada9db03230b595df41096c6de783c
Yeonji-Noob <rhcp3885@naver.com>
2023-04-04 23:45:23 +0900

    사소한 오류 수정

--- src/component/chart/chart-ranking.tsx
+++ src/component/chart/chart-ranking.tsx
@@ -1,15 +1,14 @@
 const ChartRanking: React.FC = () => {
 
 
-    let chartText = ['rank number', 'song title', 'composer'];
 
     return (
         <div className="chart-album-flax">
             <div className="chart-album-cover"></div>
             <div className="chart-album-textbox">
-                {chartText.map( item => {
-                    return (<p>{item}</p> )
-                })}
+                <p>rank number</p>
+                <p>song title</p>
+                <p>composer</p>
             </div>
         </div>
     );
